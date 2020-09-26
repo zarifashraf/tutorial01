eadbdb44591b2723531e7b8bb7ebce306b0e9a80
zarifashraf <zarif.ashraf@mail.mcgill.ca>
2020-09-26 17:24:52 +0600

    Created Student Class

---
+++ TestGit/src/test/git/Student.java
@@ -0,0 +1,19 @@
+package test.git;
+
+public class Student {
+
+	String name;
+	
+	public Student() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
